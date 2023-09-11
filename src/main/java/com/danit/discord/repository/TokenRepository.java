e7365075cc950c2970236dbf839393face42eac4
Bohdan Radchenko <radchenkobs@gmail.com>
2023-09-11 22:34:19 +0300

    save with log and depricated files not production version only for developmnet

--- src/main/java/com/danit/discord/repository/TokenRepository.java
+++ src/main/java/com/danit/discord/repository/TokenRepository.java
@@ -1,20 +1,18 @@
 package com.danit.discord.repository;
 
 import com.danit.discord.entities.Token;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface TokenRepository extends AppRepository<Token> {
-    @Query(value = """
-            select t from tokens t inner join users u\s
-            on t.user.id = u.id\s
-            where u.id = :id and (t.expired = false or t.revoked = false)\s
-            """)
-    List<Token> findAllValidTokenByUser(Integer id);
+    //    @Query(value = """
+//            select t from tokens t inner join users u\s
+//            on t.user.id = u.id\s
+//            where u.id = :id and (t.expired = false or t.revoked = false)\s
+//            """)
+//    List<Token> findAllValidTokenByUserId(Integer id);
 
     Optional<Token> findByRefreshTokenHash(String token);
 }
