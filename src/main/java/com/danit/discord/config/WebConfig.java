e8b47ce81dce12648ae52e78b8f8b4327928bf63
Bohdan Radchenko <radchenkobs@gmail.com>
2023-09-24 09:54:18 +0300

    add web mvc config cors allow all

--- src/main/java/com/danit/discord/config/WebConfig.java
+++ src/main/java/com/danit/discord/config/WebConfig.java
@@ -6,13 +6,20 @@ import org.springframework.context.annotation.ClassPathScanningCandidateComponen
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.type.filter.AnnotationTypeFilter;
 import org.springframework.web.method.HandlerTypePredicate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.lang.annotation.Annotation;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
 
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
